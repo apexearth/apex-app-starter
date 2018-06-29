c9f61aca9e726a2a78320981cce78c5bc8b5ffbb
cjacobs <cjacobs@drobo.com>
2018-06-28 18:19:58 -0700

    ...

--- app/game/index.js
+++ app/game/index.js
@@ -1,12 +1,12 @@
 const {
-          App,
-          AppObject,
-          createRenderer,
-          PIXI,
-          setting,
-          properties,
-          property
-      } = require('apex-app')
+    App,
+    AppObject,
+    createRenderer,
+    PIXI,
+    setting,
+    properties,
+    property
+} = require('apex-app')
 
 // Extend App
 // Initialize with a single AppObject drawn as a white square.
@@ -18,6 +18,23 @@ class Game extends App {
         obj.graphics.drawRect(0, 0, 10, 10)
         obj.graphics.endFill()
         this.add(obj)
+
+        this.mapping = this.createInputMapping({
+            keyboard: {
+                'createObject': 'A'
+            }
+        })
+    }
+
+    update(seconds) {
+        super.update(seconds)
+        if (this.mapping.value('createObject')) {
+            let obj = new AppObject({app: this, parent: this})
+            obj.graphics.beginFill(0xffffff, 1)
+            obj.graphics.drawRect(Math.random() * 500 - 250, Math.random() * 500 - 250, 10, 10)
+            obj.graphics.endFill()
+            this.add(obj)
+        }
     }
 }
 
